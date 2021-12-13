5da36134eac941d8caaa4dc627bdef95078ec4f5
realTechmaniac <ayomide.adebayo19@gmail.com>
2021-12-13 06:04:48 +0100

    all files added

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,73 @@
+<template>
+    <base-card>
+        <the-button @click = "setSelectedTab('stored-resources')" :mode = "storedResButtonMode">Stored Resources</the-button>
+        <the-button @click = "setSelectedTab('add-resource')" :mode = "addResButtonModes">Add Resource</the-button>
+    </base-card>
+    <keep-alive>
+        <component :is = "selectedTab"></component>
+    </keep-alive>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue'
+import AddResource from './AddResource.vue'
+export default {
+    components:{
+        StoredResources,
+        AddResource
+    },
+    data(){
+        return{
+            selectedTab : 'stored-resources',
+            storedResources : [
+                { 
+                    id: 'official-guide', 
+                    title : 'Official Guide',
+                    description : 'The official Vue JS documentation',
+                    link : 'https://vuejs.org'
+                },
+                { 
+                    id: 'google', 
+                    title : 'Google',
+                    description : 'Learn to google...',
+                    link : 'https://google.com'
+                },
+            ]
+        }
+    },
+    provide(){
+        return{
+            resources   : this.storedResources,
+            addResource : this.addResource,
+            deleteResource : this.deleteResource
+        }
+    },
+    computed :{
+        storedResButtonMode(){
+            return this.selectedTab ==='stored-resources' ? null : 'flat'
+        },
+        addResButtonMode(){
+             return this.selectedTab ==='add-resource' ? null : 'flat'
+        }
+    },
+    methods : {
+        setSelectedTab(tab){
+            this.selectedTab = tab
+        },
+        addResource(title, description, url){
+            const newResource = {
+                id         : new Date().toISOString(),
+                title      : title,
+                description: description,
+                link       : url
+            }
+            this.storedResources.unshift(newResource)   
+            this.selectedTab = 'stored-resources'
+        },
+        deleteResource(resId){
+           const resIndex = this.storedResources.findIndex(res => res.id === resId)
+           this.storedResources.splice(resIndex, 1)
+        }  
+    }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import BaseCard from './components/UI/BaseCard.vue'
+import TheButton from './components/UI/TheButton.vue'
+import BaseDialog from './components/UI/BaseDialog.vue'
+
+const app = createApp(App)
+
+app.component('base-card', BaseCard)
+
+app.component('base-dialog', BaseDialog)
+
+app.component('the-button', TheButton)
+
+app.mount('#app')
